2e45ef8ee7f9c30573424c51e181c49017753010
Blue Mountains <39657423+BlueMountainsIO@users.noreply.github.com>
2025-08-25 18:37:40 +0200

    Transfer includes_wrapper_win and includes_wrapper_mac

--- tools/make_distrib.py
+++ tools/make_distrib.py
@@ -999,9 +999,13 @@ if mode == 'standard' or mode == 'minimal':
   if platform == 'windows':
     transfer_gypi_files(cef_dir, cef_paths2['libcef_dll_wrapper_sources_win'], \
                         'libcef_dll/', libcef_dll_dir, options.quiet)
+    transfer_gypi_files(cef_dir, cef_paths2['includes_wrapper_win'], \
+                        'include/', include_dir, options.quiet)
   elif platform == 'mac':
     transfer_gypi_files(cef_dir, cef_paths2['libcef_dll_wrapper_sources_mac'], \
-                        'libcef_dll/', libcef_dll_dir, options.quiet)   
+                        'libcef_dll/', libcef_dll_dir, options.quiet)  
+    transfer_gypi_files(cef_dir, cef_paths2['includes_wrapper_mac'], \
+                        'include/', include_dir, options.quiet)   
 
   if mode == 'standard' or mode == 'minimal':
     # transfer additional files
